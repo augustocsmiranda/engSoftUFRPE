afd042d5d397331e06183fb7709abd7cdbddaf5a
augustocsmiranda <augustocezarmiranda@gmail.com>
2021-10-02 09:44:52 -0300

    Projeto Criado

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Arquivo Teste atividade 5.1 - UFRPE - Engenharia de Software");
+    }
+}
